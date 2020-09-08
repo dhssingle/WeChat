dd1439d38c386a0bba14a0279fd7f392cf575f25
Ding Haishen <dhssingle@gmail.com>
2020-09-08 17:56:48 +0800

    update

--- MiniProgram/Analysis/IAnalysisService.cs
+++ MiniProgram/Analysis/IAnalysisService.cs
@@ -5,10 +5,11 @@ namespace WeChat.MiniProgram.Analysis
     public interface IAnalysisService
     {
         Task<GetRetainResponse> GetDailyRetain(GetDailyRetainRequest request);
+        Task<GetRetainResponse> GetWeeklyRetain(GetWeeklyRetainRequest request);
+        Task<GetRetainResponse> GetMonthlyRetain(GetMonthlyRetainRequest request);
         Task<GetDailySummaryResponse> GetDailySummary(GetDailySummaryRequest request);
         Task<GetDailyVisitTrendResponse> GetDailyVisitTrend(GetDailyVisitTrendRequest request);
-        Task<GetRetainResponse> GetMonthlyRetain(GetMonthlyRetainRequest request);
-        Task<GetRetainResponse> GetWeeklyRetain(GetWeeklyRetainRequest request);
-        Task<GetWeeklyVisitTrendResponse> GetWeeklyVisitTrend(GetWeeklyRetainRequest request);
+        Task<GetWeeklyVisitTrendResponse> GetWeeklyVisitTrend(GetWeeklyVisitTrendRequest request);
+        Task<GetMonthlyVisitTrendResponse> GetMonthlyVisitTrend(GetMonthlyVisitTrendRequest request);
     }
 }
